54987effee598efec71a665a4b47fdf22bc78366
Miroslav Botur <miro@salted.cx>
2025-03-14 17:41:54 +0100

    Update seed data from local database

--- server/src/data/seeds/plates-data.ts
+++ server/src/data/seeds/plates-data.ts
@@ -1,1 +1,12 @@
-export const plates = [];
+export interface LicensePlateData {
+  _id?: string;
+  imageUrl: string;
+  description: string;
+  googleMapsUrl: string;
+  countries: string[];
+  createdAt?: string;
+  updatedAt?: string;
+  __v?: number;
+}
+
+export const plates: LicensePlateData[] = [];
