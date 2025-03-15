18c0e7bdfae4cecffd0d47a27ccd861482031410
Miroslav Botur <miro@salted.cx>
2025-03-15 13:48:11 +0100

    Update seed data from local database

--- server/src/data/seeds/plates-data.ts
+++ server/src/data/seeds/plates-data.ts
@@ -8,4 +8,127 @@ export interface LicensePlateData {
   __v?: number;
 }
 
-export const plates: LicensePlateData[] = [];
+export const plates: LicensePlateData[] = [
+  {
+    "_id": "67d56ee24560e6fa11718485",
+    "imageUrl": "/uploads/licenseplates/1742040802864-614237400.png",
+    "description": "Icelandic licence plates are fully white with blue characters. Unlike most of Europe, the plates do not feature a blue strip on the left. \r\n\r\nIt can be hard to see the blue of the characters through the blur of the plates.",
+    "countries": [
+      "a2a551a6458a8de22446cc76"
+    ],
+    "createdAt": "2025-03-15T12:13:22.875Z",
+    "updatedAt": "2025-03-15T12:13:22.875Z",
+    "__v": 0
+  },
+  {
+    "_id": "67d56f474560e6fa11718495",
+    "imageUrl": "/uploads/licenseplates/1742040903040-589850722.png",
+    "description": "Ireland has long, white licence plates with the standard European blue strip on the left.\r\n\r\nThe white backplate is especially useful to distinguish it from the UK, which uses yellow backplates (but white fronts). In addition, most UK vehicles lack the blue strip on the left.",
+    "countries": [
+      "25400724d7370b0b29c9369d"
+    ],
+    "createdAt": "2025-03-15T12:15:03.044Z",
+    "updatedAt": "2025-03-15T12:15:03.044Z",
+    "__v": 0
+  },
+  {
+    "_id": "67d5738b8e22b0eedb071ced",
+    "imageUrl": "/uploads/licenseplates/1742041995006-913547559.png",
+    "description": "UK front plates are white, while back plates are yellow. Some plates have a blue strip on the left, but most do not.\r\n\r\nAdditionally, you might see a plate with a green ‘Zero Emissions’ strip.\r\n\r\nNOTE: Ireland uses white front and back plates, always with a blue strip on the left.",
+    "countries": [
+      "7885444af42e4b30c518c5be"
+    ],
+    "createdAt": "2025-03-15T12:33:15.010Z",
+    "updatedAt": "2025-03-15T12:33:15.010Z",
+    "__v": 0
+  },
+  {
+    "_id": "67d574368e22b0eedb071da6",
+    "imageUrl": "/uploads/licenseplates/1742042166337-188521563.png",
+    "description": "Portuguese plates are long and white, with a blue strip on the left. Most plates have a yellow strip on the right, which is unique to Portugal. \r\n\r\nThe yellow strip was discontinued in 2020, so in Generation 4 coverage you may sometimes see plates with just the blue strip.",
+    "countries": [
+      "fc9fdf084e290f26a270390d"
+    ],
+    "createdAt": "2025-03-15T12:36:06.339Z",
+    "updatedAt": "2025-03-15T12:36:06.339Z",
+    "__v": 0
+  },
+  {
+    "_id": "67d574b98e22b0eedb071daa",
+    "imageUrl": "/uploads/licenseplates/1742042297615-798879768.png",
+    "description": "Modern French plates are long and white, with blue strips on both sides of the plate. The left strip will be more visible than the right strip.\r\n\r\nSome older French plates lack the right blue strip, with the rear plate being yellow. These are very rare in newer coverage.\r\n\r\nNOTE: Italian plates also have two blue strips, which are equally visible. In addition, Italian front plates are considerably shorter.",
+    "countries": [
+      "82a9e4d26595c87ab6e44239"
+    ],
+    "createdAt": "2025-03-15T12:38:17.618Z",
+    "updatedAt": "2025-03-15T12:38:17.618Z",
+    "__v": 0
+  },
+  {
+    "_id": "67d574f78e22b0eedb071dae",
+    "imageUrl": "/uploads/licenseplates/1742042359196-192427731.png",
+    "description": "Licence plates in Belgium use red text. You can recognise this distinct colour even through the blur.",
+    "countries": [
+      "910955a907e739b81ec88557"
+    ],
+    "createdAt": "2025-03-15T12:39:19.198Z",
+    "updatedAt": "2025-03-15T12:39:19.198Z",
+    "__v": 0
+  },
+  {
+    "_id": "67d575a18e22b0eedb071db2",
+    "imageUrl": "/uploads/licenseplates/1742042529649-154347521.png",
+    "description": "The Netherlands has long, yellow licence plates with the standard European blue strip on the left. Dutch taxis have blue plates.\r\n\r\nNOTE: The only other countries in Europe with yellow licence plates are Luxembourg and the UK. For the UK, only the back plates are yellow, and they often lack the blue strip. In addition, some older coverage in France features the French yellow back plates. In Denmark, you can find yellow plates on commercial vehicles.\r\n\r\nLux plates have a different format but blurred it's not recognisable. ",
+    "countries": [
+      "3e7e122bf08f48432c961ba4",
+      "1a13105b7e4eb5fb2e7c9515"
+    ],
+    "createdAt": "2025-03-15T12:42:09.653Z",
+    "updatedAt": "2025-03-15T12:42:09.653Z",
+    "__v": 0
+  },
+  {
+    "_id": "67d576068e22b0eedb071db6",
+    "imageUrl": "/uploads/licenseplates/1742042630775-514521788.png",
+    "description": "Swiss licence plates differ substantially from other European plates. They are fully white and lack the blue strip found in most other European countries.\r\n\r\nFront plates are extremely short compared to most in Europe. \r\n\r\nRear plates can be either long or tall. In both cases the red country emblem can be seen on the left, while the canton emblem can be seen on the right.",
+    "countries": [
+      "d88fc6edf21ea464d35ff762"
+    ],
+    "createdAt": "2025-03-15T12:43:50.777Z",
+    "updatedAt": "2025-03-15T12:43:50.777Z",
+    "__v": 0
+  },
+  {
+    "_id": "67d5764d8e22b0eedb071dba",
+    "imageUrl": "/uploads/licenseplates/1742042701362-239437771.png",
+    "description": "Contrary to most European licence plates, Italian plates have blue strips on either side of the plate. Furthermore, the front plate is distinctly shorter than most European plates, which in combination with the two strips, makes it unique.\r\n\r\nNOTE: Albanian licence plates also have two blue strips, and French licence plates have a smaller strip on the right. However, neither Albanian nor French front plates are short.",
+    "countries": [
+      "0d149b90e7394297301c9019"
+    ],
+    "createdAt": "2025-03-15T12:45:01.364Z",
+    "updatedAt": "2025-03-15T12:45:01.364Z",
+    "__v": 0
+  },
+  {
+    "_id": "67d576868e22b0eedb071dbe",
+    "imageUrl": "/uploads/licenseplates/1742042758541-430513837.png",
+    "description": "Andorran licence plates have an orange dot on the left side and can be regularly found throughout the country.\r\n\r\nNOTE: Within Europe, this plate is unique to Andorra.",
+    "countries": [
+      "523af537946b79c4f8369ed3"
+    ],
+    "createdAt": "2025-03-15T12:45:58.544Z",
+    "updatedAt": "2025-03-15T12:45:58.544Z",
+    "__v": 0
+  },
+  {
+    "_id": "67d576c68e22b0eedb071dc2",
+    "imageUrl": "/uploads/licenseplates/1742042822719-865848012.png",
+    "description": "Norway has long white licence plates with the standard European strip on the left.\r\n\r\nCommercial plates are green. These are often seen on vans.\r\n\r\nNOTE: Within Europe, green licence plates are most commonly seen in Norway, but also on electric vehicles in Hungary.",
+    "countries": [
+      "7fa3b767c460b54a2be4d490"
+    ],
+    "createdAt": "2025-03-15T12:47:02.722Z",
+    "updatedAt": "2025-03-15T12:47:02.722Z",
+    "__v": 0
+  }
+];
